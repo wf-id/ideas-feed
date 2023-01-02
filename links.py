f0de5bfaee9831de04d55ba78f79c071425f2c04
Michael DeWitt <me.dewitt.jr@gmail.com>
2023-01-02 00:20:30 -0500

    update

--- links.py
+++ links.py
@@ -31,7 +31,7 @@ headers = {
 }
 
 outinfo = []
-for i in range(len(links)):
+for i in range(0):
     body = {'data-raw': links[0] }
     r = requests.post(url, data=body, headers=headers)
     outinfo = outinfo + json.loads(r.content.decode("utf-8"))
@@ -39,3 +39,29 @@ for i in range(len(links)):
 with open('bibinfo.json', 'w') as out_file:
      json.dump(outinfo, out_file, sort_keys = True, indent = 4,
                ensure_ascii = False)
+
+
+url2 = 'https://t0guvf0w17.execute-api.us-east-1.amazonaws.com/Prod/export?format=bibtex'
+
+headers2 = {
+  'authority': 't0guvf0w17.execute-api.us-east-1.amazonaws.com' ,
+  'accept': '*/*' ,
+   'accept-language': 'en-US,en;q=0.9' ,
+   'content-type': 'application/json' ,
+   'dnt': '1' ,
+   'origin': 'https://zbib.org' ,
+   'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"' ,
+   'sec-ch-ua-mobile': '?0' ,
+   'sec-ch-ua-platform': '"macOS"' ,
+   'sec-fetch-dest': 'empty' ,
+   'sec-fetch-mode': 'cors' ,
+   'sec-fetch-site': 'cross-site' ,
+   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' 
+}
+
+
+outinfo = str(outinfo).replace("'", '"')
+
+body2 = {'data-raw': outinfo }
+
+r = requests.post(url2, data=body2, headers=headers2)
